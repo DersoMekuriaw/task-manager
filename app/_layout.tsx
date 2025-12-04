9fa457166d8c321c4ab045fd2bcb8bb639641772
Nolawi <derso2009@gmail.com>
2025-12-03 22:43:33 -0900

    UI updated, new task form added, camera implementation add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,32 @@
 import { Stack } from "expo-router";
+import "./globals.css";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Tasks",
+          headerBackButtonDisplayMode: "minimal",
+        }}
+      />
+
       <Stack.Screen
         name="details"
         options={{
-          title: "Details",
+          title: "Task Details",
           headerBackButtonDisplayMode: "minimal",
           // presentation: "formSheet",
           // sheetAllowedDetents: [0.3, 0.5, 0.8],
+        }}
+      />
+
+      <Stack.Screen
+        name="new"
+        options={{
+          title: "New Task",
+          headerBackButtonDisplayMode: "minimal",
         }}
       />
     </Stack>
